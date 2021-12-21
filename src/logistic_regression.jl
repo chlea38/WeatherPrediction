f9803d8e112adbfacacc28da4dab43b812b7febf
chlea38 <79040716+chlea38@users.noreply.github.com>
2021-12-21 16:19:05 +0100

    Correct a small error

--- src/logistic_regression.jl
+++ src/logistic_regression.jl
@@ -9,16 +9,12 @@ begin
     using InteractiveUtils
     using Pkg
     Pkg.activate(joinpath(Pkg.devdir(), "MLCourse"))
-    #import GLMNet: glmnet
-    #using StatsPlots
-    #using MLJLinearModels
     using DataFrames, CSV, MLJ, OpenML, MLJLinearModels, Statistics, MLCourse, LinearAlgebra, Random
 end
 
 # Import training & test data and deal with missing data with FillImputer
 begin
 	weather = CSV.read(joinpath(@__DIR__, "..",  "data", "trainingdata.csv"), DataFrame)
-    #coerce!(weather, :precipitation_nextday=>Multiclass)
     coerce!(weather, :precipitation_nextday=>OrderedFactor)
     weather_filled = MLJ.transform(fit!(machine(FillImputer(), weather)), weather) #Filling imputer data during the fit 
 end
@@ -31,8 +27,6 @@ end
 begin
     test_data = CSV.read(joinpath(@__DIR__, "..",  "data", "testdata.csv"), DataFrame)
 	cleaned_test_data = MLJ.transform(fit!(machine(FillImputer(), test_data)), test_data)
-    #cleaned_test_data = dropmissing(test_data)
-    #stan_test_data = MLJ.transform(stan_mach,cleaned_test_data)
 end
 
 # Apply multiple logistic regression
@@ -46,10 +40,9 @@ begin
                                    range = range(model, :lambda, values = lambda),
                                    measure = auc)
 	selftuning_lambda_mach = fit!(machine(selftuning_lambda, input, output))
-    # avec PCA, donne des résultats bien pire ...
+    # PCA gives far worse results :
     # selftuning_lambda_mach = fit!(machine(@pipeline(PCA(), selftuning_lambda), input,output))
 end
-confusion_matrix(predict_mode(selftuning_lambda_mach, input), output)
 
 # Evaluation with AUC and confusion matrix on full input set
 #auc_val = MLJ.auc(predict(selftuning_lambda_mach,input).prob_given_ref.vals[2],output)
@@ -58,7 +51,7 @@ confusion_matrix(predict_mode(selftuning_lambda_mach, input), output)
 
 # Save results in CSV file
 begin
-	probs_L2 = MLJ.predict(selftuning_lambda_mach, stan_test_data).prob_given_ref.vals
+	probs_L2 = MLJ.predict(selftuning_lambda_mach, cleaned_test_data).prob_given_ref.vals
 	N = size(cleaned_test_data)[1]
 	df_L2 = DataFrame(id = 1:N, precipitation_nextday = probs_L2[2])
     CSV.write(joinpath(@__DIR__, "..", "results", "logistic_selftuned_L2_regression.csv"), df_L2)
